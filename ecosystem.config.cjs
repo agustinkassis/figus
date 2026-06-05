442d634124e70ecc1555943abd07732948f6a142
Negr087 <spibanezpaypal@gmail.com>
2026-06-05 21:35:16 +0000

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: "issuer",
-      script: "./node_modules/.bin/tsx",
+      script: "./node_modules/.bin/tsx.cmd",
       args: "issuer/index.ts",
-      interpreter: "node",
       watch: false,
       env_file: ".env.local",
     },
